8cdfc549efe5dd5269c1cde256609f79bbc61bec
phuc-2561 <duong.ngoc.phuc@sun-asterisk.com>
2023-09-24 23:14:01 +0700

    section 8 api response use class

--- src/core/success.response.js
+++ src/core/success.response.js
@@ -1,2 +1,44 @@
-'use strict';
+"use strict";
 
+const { StatusCodes, ReasonPhrases } = require("../constants/httpStatusCode");
+
+class SuccessResponse {
+  constructor({
+    message,
+    statusCode = StatusCodes.OK,
+    reasonStatusCode = ReasonPhrases.OK,
+    metadata = {},
+  }) {
+    this.message = !message ? reasonStatusCode : message;
+    this.status = statusCode;
+    this.metadata = metadata;
+  }
+
+  send(response, headers = {}) {
+    return response.status(this.status).json(this);
+  }
+}
+
+class OK extends SuccessResponse {
+  constructor({ message, metadata }) {
+    super({ message, metadata });
+  }
+}
+
+class CREATED extends SuccessResponse {
+  constructor({
+    message,
+    statusCode = StatusCodes.CREATED,
+    reasonStatusCode = ReasonPhrases.CREATED,
+    metadata,
+    options = {},
+  }) {
+    super({ message, statusCode, reasonStatusCode, metadata });
+    this.options = options;
+  }
+}
+
+module.exports = {
+  OK,
+  CREATED,
+};
