7a8550aaaa05ec984be337ef5e30a41d0605c41c
phuc-2561 <duong.ngoc.phuc@sun-asterisk.com>
2023-09-26 23:26:12 +0700

    section 10 logout

--- src/services/keyToken.service.js
+++ src/services/keyToken.service.js
@@ -1,7 +1,9 @@
 "use strict";
 
 const keytokenModel = require("../models/keytoken.model");
-
+const {
+  Types: { ObjectId },
+} = require("mongoose");
 class KeyTokenService {
   static createKeyToken = async ({
     userId,
@@ -35,6 +37,16 @@ class KeyTokenService {
     } catch (error) {
       return error;
     }
+  };
+
+  static findByUserId = async (userId) => {
+    return await keytokenModel.findOne({ user: new ObjectId(userId) }).lean();
+  };
+
+  static removeTokenById = async (id) => {
+    return await keytokenModel.deleteOne({
+      _id: new ObjectId(id),
+    });
   };
 }
 
